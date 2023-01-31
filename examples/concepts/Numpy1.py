f72bc7eb3013599e7ad6a65963dc670a45f4d655
itasworkshop <itasworkshop@gmail.com>
2023-01-31 15:55:08 +0530

    python pandas 1

--- examples/concepts/Numpy1.py
+++ examples/concepts/Numpy1.py
@@ -4,21 +4,23 @@ import numpy as np
 from numpy import longdouble, int64
 from numpy.compat import long
 
-myarr = np.array([1,2,3,45,7])
-#myarr = np.array([[1,2,3],[4,5,6]])
+#myarr = np.array([1,2,3,45,7])
+myarr = np.array([[11,20,3],[40,5,6]])
 print(type(myarr))
 print(myarr.dtype)
 print(myarr.ndim)
 
 print(np.sum(myarr))
+#print(np.sort(myarr,axis=0)) for column
+print(np.sort(myarr,axis=None))
 
 myarr*=2
-print(myarr)
+#print(myarr)
 
 #[111,'Rajesh',2016-12-25,5000]
 
 dtypes = [('id',int),('name','<U9'),('Join_Date',date),('salary',int64)]
-data = [(111,'Rajesh','2016-12-25',5000),(222,'Suraj','2016-12-25',70000000000)]
+data = [(555,'Kai','2016-12-25',5000),(222,'Aman','2016-12-25',700)]
 
 emparr = np.array(data,dtypes)
-print(emparr)
+print(np.sort(emparr,order=('name','salary')))
---
+++ examples/concepts/Pandas1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = [(555,'Kai','2016-12-25',5000),(222,'Aman','2016-12-25',700)]
+
+df = pd.DataFrame(data)
+print(df)
