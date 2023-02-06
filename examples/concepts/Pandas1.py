98236a7c43faaaf4f096d573ee4bb58ef14c1c75
itasworkshop <itasworkshop@gmail.com>
2023-02-06 09:55:17 +0530

    python pyspark 1

--- examples/concepts/Pandas1.py
+++ examples/concepts/Pandas1.py
@@ -3,4 +3,10 @@ import pandas as pd
 data = [(555,'Kai','2016-12-25',5000),(222,'Aman','2016-12-25',700)]
 
 df = pd.DataFrame(data)
-print(df)
+#print(df)
+
+stdf1 = pd.read_csv('C:/Development/AI_Bootcamp_Batch_C/data/student.csv',header=None)
+print(stdf1)
+
+stdf2 = pd.read_csv('C:/Development/AI_Bootcamp_Batch_C/data/student.csv',names=['id','name','grade'])
+print(stdf2)
